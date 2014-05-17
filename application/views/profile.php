95348e6f8fd369eb6311a937308ab68b0fce910f
sreekeshkamath <sreekeshkamath95@gmail.com>
2014-05-18 03:06:55 +0530

    profile

---
+++ application/views/profile.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<div class="upper">
+<div class="profile">
+<h2>Profile</h2>
+<img src="defaultpic.png" alt="Profile Picture" width=80 height=80/>
+</div>
+<div class="profile_details">
+Name:
+email:
+
+</div>
+</div>
+</body>
+</html>
