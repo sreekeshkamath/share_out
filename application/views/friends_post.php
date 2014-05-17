92e99656ef46752f3b03c1a79b4e53e5c571bf13
sreekeshkamath <sreekeshkamath95@gmail.com>
2014-05-18 02:06:57 +0530

    css styles added and header

--- application/views/friends_post.php
+++ application/views/friends_post.php
@@ -1,22 +1,7 @@
 <html>
-
 <body>
-
-<h1>ShareOut!</h1>
-
-<nav>
-  <ul>
-      <li><a href="friends_post.php">Friends Post</a>
-	  <li><a href="interests_post.php">Interests Post</a>
-	  <li><a href="share_out.php">ShareOut</a>
-	  <li><a href="#">Profile</a>
-	  </ul>
-</nav>
-
-<h2>Friends Post<h2>
-
 <div class="friendspost">
-
+<h2>Friends Post<h2>
 	<img src="" alt="Profile Picture" name="profile_pic" />
 <p name="post_details"></p> 
 <img src="" alt="profile_pic" width=40 height=40/>
--- application/views/interests_post.php
+++ application/views/interests_post.php
@@ -1,22 +1,7 @@
 <html>
-
 <body>
-
-<h1>ShareOut!</h1>
-
-<nav>
-  <ul>
-      <li><a href="friends_post.php">Friends Post</a>
-	  <li><a href="interests_post.php">Interests Post</a>
-	  <li><a href="share_out.php">ShareOut</a>
-	  <li><a href="#">Profile</a>
-	  </ul>
-</nav>
-
-<h2>Interests Post</h2>
-
 <div class="interests_post">
-
+<h2>Interests Post</h2>
 <img src="" alt="profile_pic" width=40 height=40/>
 <div class="image"><img src="" alt="details"></div>
 <div class="otherposts"></div>
