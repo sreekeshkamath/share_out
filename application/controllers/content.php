59fc203b5281b952f15dce3206b754ee32e94649
Athul Ram <vrathulram@gmail.com>
2014-05-18 01:26:27 +0530

    Damn bugs

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -28,14 +28,53 @@ class Content extends MY_Controller {
 		else
 		{
 			$info = array('title' => $this->form_validation->set_value('title'),
-						'description' => $this->form_validation->set_value('description'));
-			redirect('content/do_upload');
+						'description' => $this->form_validation->set_value('description'),
+						'user_id' => $this->control->is_logged_in()
+						);
+						
+			if ($this->form_validation->set_value('type') == '1') {
+				// Temp code, THIS IS A VERY BAD IDEA to pass the file name
+				$info['picture'] = uniqid().'.jpg';
+				$this->db->insert('ads', $info);
+				$this->session->set_userdata('filename', $info['picture']);
+				echo 'GOD';
+				//~ $this->load->view('upload_form');
+			} else if ($this->form_validation->set_value('type') == '2') {
+				$this->db->insert('ideas', $info);
+				echo 'Successfully posted new idea.<br />';
+			}
 		}
 	}
 	
-	public function do_upload($info)
+	public function do_upload()
 	{
-		if ( ! is_array($info)) echo 'No direct access allowed.';
+		
+		echo $this->session->userdata('filename');
+		$this->load->helper('form');
+		
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['file_name'] = $this->session->userdata('filename');
+		$config['max_size']	= '100';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_form', $error);
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			$this->session->unset_userdata('filename');
+			echo 'Successfully added new ad';
+		}
+		
 	}
 	
 	public function _check_type($str)
--- application/views/share_out.php
+++ application/views/share_out.php
@@ -14,7 +14,7 @@
 <h2>Go ahead post anything...</h2>
 
 <?php echo validation_errors(); ?>
-<?php echo form_open_multipart('content/add_post', array('class'=>'post_anything')) ?>
+<?php echo form_open_multipart('content/do_upload', array('class'=>'post_anything')) ?>
 
 <select name="type">
 	<option value="0" selected="selected">Select a type</option>
@@ -26,7 +26,7 @@ Title:
 <input type="text" name="title" value="<?php echo set_value('title'); ?>"/>
 <br>
 Description:<br />
-<textarea name="description" value="<?php echo set_value('description');?>" cols=50 rows=5 placeholder="Please give a description..."></textarea>
+<textarea name="description" cols=50 rows=5 placeholder="Please give a description..."><?php echo set_value('description');?></textarea>
 <br>
 
 <input type="submit" value="Post"/>
