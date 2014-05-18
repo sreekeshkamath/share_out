814c480aac0cce760491edbb389915dc71ffa2a3
sreekeshkamath <sreekeshkamath95@gmail.com>
2014-05-18 05:35:49 +0530

    CSS added to friends post and profile

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,23 +1,22 @@
 <html>
 <body>
-
 <div class="upper">
-<div class="profile">
+<div class="profile_picture">
 <h2>Profile</h2>
 <img src="/share_out/uploads/defaultpic.png" alt="Profile Picture" width=80 height=80/>
 </div>
 <div class="profile_details">
 Name: <?php echo $username; ?> <br />
 email: <?php echo $email; ?> <br />
-
-
+</div>
+<div class="followers">
 <?php 
  if ( ! $own_page) {
 if ($following)
 { ?>
-	<a href="<?php echo site_url('profile/unfollow/'.$user_id);?>">Unfollow</a>
+	<a href="<?php echo site_url('profile/unfollow/'.$user_id);?>" class="buttonb1">Unfollow</a>
 <?php } else { ?>
-	<a href="<?php echo site_url('profile/follow/'.$user_id);?>">Follow</a>
+	<a href="<?php echo site_url('profile/follow/'.$user_id);?>" class = "buttonb2">Follow</a>
 <?php } }
 ?>
 </div>
