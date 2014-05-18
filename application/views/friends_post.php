64028c9c994bc7edd6a18b1b59f69cadd6537795
sreekeshkamath <sreekeshkamath95@gmail.com>
2014-05-18 06:10:49 +0530

    friendpost version 2

--- application/views/friends_post.php
+++ application/views/friends_post.php
@@ -1,11 +1,23 @@
 <html>
 <body>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<h1 class="heading background">ShareOut!</h1>
 
+<nav>
+  <ul>
+      <li><a href="friends_post.php">Friends Post</a>
+	  <li><a href="interests_post.php">Interests Post</a>
+	  <li><a href="share_out.php">ShareOut</a>
+	  <li><a href="#">Profile</a>
+	  </ul>
+</nav>
+<div class="main">
 <h2>Friend's Post</h2>
 <div class="posts">
 <img src="defaultpic.png" width=20px height=20px />
-'Profile name'<br>
+<br>'Profile name'<br>
 Title<br>
 Details
+</div>
 </body>
 </html>
